842ed85639f242eded1ba6e9ad20996c991e6518
rahulbari <rahulbari717@gmail.com>
2024-03-18 11:44:31 +0530

    7_capturing

---
+++ 7_capturing_input/main/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Enter your name : ");
+    char c = 0;
+    char str[100];
+    memset(str, 0, sizeof(str));
+    while (c != '\n')
+    {
+        c = getchar();
+        if (c != 0xff)
+        {
+            str[strlen(str)] = c;
+            printf("%c", c);
+        }
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+
+    printf("Your name is : %s\n", str);
+}
